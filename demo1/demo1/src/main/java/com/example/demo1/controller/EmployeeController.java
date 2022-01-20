f8924eb6ace7e9f020331312c8c312c37db9e6ad
Leow0069 <hansen_leow@hotmail.com>
2022-01-20 17:57:48 +0800

    Upload files for demo1

---
+++ demo1/demo1/src/main/java/com/example/demo1/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.example.demo1.controller;
+
+import com.example.demo1.model.Employee;
+import com.example.demo1.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
